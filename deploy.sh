b4ce419cf2ee8db2e9f63b75dae5518dc22505aa
Shawn Seymour <shawn@devshawn.com>
2017-10-13 22:32:13 -0500

    [#3] - Adding missing environment variables

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $REGISTRY_HOST
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
 docker pull $REGISTRY_HOST/$IMAGE_NAME
-docker run -d --name $CONTAINER_NAME -e VIRTUAL_HOST=staging.password.live $REGISTRY_HOST/$IMAGE_NAME
+docker run -d --name $CONTAINER_NAME -e VIRTUAL_HOST=staging.password.live -e PORT=8080 -e NODE_ENV=staging $REGISTRY_HOST/$IMAGE_NAME
 ENDSSH
