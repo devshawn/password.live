18da3e91df1f13a7666a84b6596c00b7ace72870
Shawn Seymour <shawn@devshawn.com>
2017-10-13 22:12:03 -0500

    [#3] - Attempting deploy to staging

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+echo "Running deploy"
+
+ssh root@staging.password.live << ENDSSH
+docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $REGISTRY_HOST
+docker stop $CONTAINER_NAME
+docker rm $CONTAINER_NAME
+docker pull $REGISTRY_HOST/$IMAGE_NAME
+docker run -d --name $CONTAINER_NAME -e VIRTUAL_HOST=staging.password.live $REGISTRY_HOST/$IMAGE_NAME
+ENDSSH
