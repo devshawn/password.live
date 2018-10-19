15023e1bf10b62585c54a30b92b85768e2c3323b
Shawn Seymour <shawn@devshawn.com>
2018-10-19 16:49:50 -0500

    Moving repository to GitHub

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-echo "Running deploy"
-
-ssh root@staging.password.live << ENDSSH
-docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $REGISTRY_HOST
-docker stop $CONTAINER_NAME
-docker rm $CONTAINER_NAME
-docker pull $REGISTRY_HOST/$IMAGE_NAME
-docker run -d --name $CONTAINER_NAME -e VIRTUAL_HOST=staging.password.live -e PORT=8080 -e NODE_ENV=staging $REGISTRY_HOST/$IMAGE_NAME
-ENDSSH
--- server/utilities/logger.js
+++ server/utilities/logger.js
@@ -1,20 +1,5 @@
 export class Logger {
 
-    static debug(message, obj) {
-        console.log(`[DEBUG] ${message}`, obj)
-    }
-
-    static debugGeneration(message, options, password) {
-        console.log("---------- Password Generation Debug ----------")
-        console.log(`Regex: ${message}`)
-        console.log(`Password: ${password}`)
-        const date = new Date()
-        console.log(`Time: `, date.toDateString(), date.toTimeString())
-        console.log(`Options:`)
-        console.log(options)
-        console.log("-----------------------------------------------")
-    }
-
     static info(message) {
         console.log(`[INFO] ${message}`)
     }
