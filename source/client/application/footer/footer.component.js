26e9d0f7da07cecbb1f0b46641619d680bc9565e
Philip Plagge <sayhi@itscursedphil.com>
2018-10-23 21:05:33 +0200

    Renamed footer.gitlab.icon.js to footer.github.icon.js and changed association in footer.component.js

--- source/client/application/footer/footer.component.js
+++ source/client/application/footer/footer.component.js
@@ -1,19 +1,22 @@
-import React from "react"
-import { footerLeftStyle, footerRightStyle, footerStyle } from "./footer.styles"
-import { FooterGitlabIcon } from "./footer.gitlab.icon"
+import React from "react";
+import {
+  footerLeftStyle,
+  footerRightStyle,
+  footerStyle
+} from "./footer.styles";
+import { FooterGithubIcon } from "./footer.github.icon";
 
 export class FooterComponent extends React.Component {
-    render() {
-        return (
-            <div style={ footerStyle }>
-                <div style={ footerLeftStyle }>
-                    Copyright &copy; {(new Date()).getFullYear()}
-                </div>
-                <div style={ footerRightStyle }>
-                    <FooterGitlabIcon/>
-                </div>
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div style={footerStyle}>
+        <div style={footerLeftStyle}>
+          Copyright &copy; {new Date().getFullYear()}
+        </div>
+        <div style={footerRightStyle}>
+          <FooterGithubIcon />
+        </div>
+      </div>
+    );
+  }
 }
