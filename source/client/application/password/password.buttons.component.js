b704e96b049813ce257eafa99c3cac9096ff7c38
186866380 <olugbenro.selere@rbc.com>
2018-10-25 09:11:44 -0400

    updated styling for password box & button components

--- source/client/application/password/password.buttons.component.js
+++ source/client/application/password/password.buttons.component.js
@@ -2,7 +2,7 @@ import React from "react"
 import copy from "copy-to-clipboard"
 import { connect } from "react-redux"
 import Button from "@material-ui/core/Button"
-import { buttonStyle, buttonLabelStyle, passwordButtonsComponentStyle } from "../styles"
+import { buttonStyle, buttonLabelStyle, passwordButtonsComponentStyle, copyPasswordButtonStyle } from "../styles"
 import { sendNotification } from "../notification/notification.actions"
 import { updatePasswordSettings } from "../settings/settings.actions"
 
@@ -37,22 +37,16 @@ export class PasswordButtonsComponent extends React.Component {
                 <Button onClick={generatePassword}
                     color="primary"
                     variant="contained"
-                    // backgroundColor={lightBlue500}
-                    // label="Generate Password"
-                    // labelStyle={buttonLabelStyle}
                     style={buttonStyle}
                 >
-                    Generate Password
-                    </Button>
+                    <label style={buttonLabelStyle}>Generate Password</label>
+                </Button>
                 <Button onClick={this.copyPassword}
                     color="secondary"
                     variant="contained"
-                    // backgroundColor="#F18A00"
-                    // label="Copy Password"
-                    // labelStyle={buttonLabelStyle}
                     style={copyPasswordButtonStyle}
                 >
-                    Copy Password
+                    <label style={buttonLabelStyle}>Copy Password</label>
                 </Button>
             </div>
         )
