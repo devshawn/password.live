5694255fef2b960187ee592e3dc5713cf7040dd8
Shawn Seymour <shawn@devshawn.com>
2018-10-25 13:22:58 -0500

    Fix broken password controller test

--- source/server/__tests__/controllers/password.controller.spec.js
+++ source/server/__tests__/controllers/password.controller.spec.js
@@ -5,7 +5,7 @@ const application = new Application().app
 
 describe("password controller", () => {
     it("generate responds with a 200", async () => {
-        const response = await request(application).post("/password/generate")
+        const response = await request(application).post("/api/v1/password")
         expect(response.statusCode).toBe(200)
     })
 })
