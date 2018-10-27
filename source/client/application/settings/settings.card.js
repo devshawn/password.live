e3fe179b85c413a975e33a1319d57743508c131c
186866380 <olugbenro.selere@rbc.com>
2018-10-27 01:36:01 -0400

    completed material ui-update

--- source/client/application/settings/settings.card.js
+++ source/client/application/settings/settings.card.js
@@ -1,20 +1,20 @@
 import React from "react"
-import { Avatar, Card, CardHeader, CardText, FontIcon } from "material-ui"
-import { lightBlue500 } from "material-ui/styles/colors"
+import { Avatar, Card, CardHeader, CardContent, Icon } from "@material-ui/core"
+import { lightBlue } from "@material-ui/core/colors"
 import { shieldIconStyle } from "./settings.styles"
 import { SettingsCardItems } from "./settings.card.items"
 
 export class SettingsCard extends React.Component {
 
     render() {
-        const avatar = <Avatar backgroundColor={ lightBlue500 } icon={ <FontIcon style={ shieldIconStyle } className="fa fa-shield" aria-hidden="true"/> }/>
+        const avatar = <Avatar style={{ backgroundColor: lightBlue[500] }}><Icon style={shieldIconStyle} className="fa fa-shield" aria-hidden="true" /></Avatar>
 
         return (
             <Card>
-                <CardHeader title="Password Settings" subtitle="Customize your generated password" avatar={ avatar }/>
-                <CardText>
-                    <SettingsCardItems/>
-                </CardText>
+                <CardHeader title="Password Settings" subheader="Customize your generated password" avatar={avatar} />
+                <CardContent>
+                    <SettingsCardItems />
+                </CardContent>
             </Card>
         )
     }
