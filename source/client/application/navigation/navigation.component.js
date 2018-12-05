1af1233d93c7e5a81f88125a6095c14456d30a16
Shawn Seymour <shawn@devshawn.com>
2018-12-04 21:07:00 -0600

    Fixing navigation bar and links

--- source/client/application/navigation/navigation.component.js
+++ source/client/application/navigation/navigation.component.js
@@ -1,12 +1,11 @@
 import React from "react"
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom"
 import AppBar from "@material-ui/core/AppBar"
 import Icon from "@material-ui/core/Icon"
 import Tab from "@material-ui/core/Tab"
 import Tabs from "@material-ui/core/Tabs"
-import Toolbar from "@material-ui/core/Toolbar"
 import Typography from "@material-ui/core/Typography"
-import { appBarStyle, appBarTitleStyle, iconStyle, inkBarStyle, tabItemContainerStyle, tabItemStyle, appToolBarStyle } from "./navigation.styles"
+import { appBarTitleStyle, iconStyle, tabItemContainerStyle, tabItemStyle, appToolBarStyle } from "./navigation.styles"
 
 const tabItemContainerStyles = tabItemContainerStyle(2)
 
@@ -18,34 +17,31 @@ export class NavigationComponent extends React.Component {
     }
 
     handleNavigation(event, value) {
-        event.preventDefault();
-        let historyValue = location.pathname;
-        this.props.history.push(historyValue)
+        this.props.history.push(value)
         this.setState({ value: value })
     }
 
     render() {
         const { value } = this.state
-        console.log(this.state);
 
         return (
-            <AppBar position="static" style={appToolBarStyle}>
-                <div style={{ display: "flex", flexGrow: 2 }}>
-                    <Icon style={iconStyle} className="fa fa-user-secret" aria-hidden="true"></Icon>
-                    <Typography variant="h6" style={appBarTitleStyle} color="inherit">
+            <AppBar position="static" style={ appToolBarStyle }>
+                <div style={ { display: "flex", flexGrow: 2 } }>
+                    <Icon style={ iconStyle } className="fa fa-user-secret" aria-hidden="true"/>
+                    <Typography variant="h6" style={ appBarTitleStyle } color="inherit">
                         Password Generator
-                </Typography>
+                    </Typography>
                 </div>
-                <div style={{ marginLeft: "auto" }}>
+                <div style={ { marginLeft: "auto" } }>
                     <Tabs
                         fullWidth
-                        value={value}
-                        onChange={this.handleNavigation}
-                        style={tabItemContainerStyles}
+                        value={ value }
+                        onChange={ this.handleNavigation }
+                        style={ tabItemContainerStyles }
                         indicatorColor="primary"
                     >
-                        <Tab key={"/"} label={"Home"} style={tabItemStyle} value={0} component={Link} to={"/"} />
-                        <Tab key={"/learn"} label={"Learn"} style={tabItemStyle} value={1} component={Link} to={"/learn"} />
+                        <Tab key={ "/" } label={ "Home" } style={ tabItemStyle } value={ 0 } component={ Link } to={ "/" }/>
+                        <Tab key={ "/learn" } label={ "Learn" } style={ tabItemStyle } value={ 1 } component={ Link } to={ "/learn" }/>
                     </Tabs>
                 </div>
             </AppBar>
